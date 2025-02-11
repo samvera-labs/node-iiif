f788a60e4e9a6f033d3cb93cf4dca715bc0288a9
Michael B. Klein <mbklein@gmail.com>
2025-02-11 15:53:06 -0600

    Add Clover IIIF manifest viewer and sample manifest/images

--- examples/tiny-iiif/index.js
+++ examples/tiny-iiif/index.js
@@ -6,6 +6,13 @@ import serveStatic from 'serve-static';
 import { port, iiifImagePath } from './config.js';
 import IIIFRouter from './iiif.js';
 
+const setHeaders = (res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Credentials', 'false');
+}
+
 const app = new App({ settings: { networkExtensions: true } });
 
 app
@@ -14,5 +21,5 @@ app
   .use('/iiif/3', IIIFRouter(3))
   .use('/openseadragon', serveStatic('./node_modules/openseadragon/build/openseadragon'))
   .use('/images', serveStatic('./node_modules/openseadragon/build/openseadragon/images'))
-  .use('/', serveStatic('./public'))
+  .use('/', serveStatic('./public', { setHeaders }))
   .listen(port, () => console.log(`Serving IIIF images from ${iiifImagePath} on http://localhost:${port}`));
